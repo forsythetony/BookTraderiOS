1b5a3610b01cc315ac601a763c611c8cf92c8faf
Tony Forsythe <forsythetony@gmail.com>
2016-10-14 21:42:24 -0500

    "Adds some stuff to the storyboard and adds a git ignore file"

---
+++ BookTrader/LandingPageViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LandingPageViewController.swift
+//  BookTrader
+//
+//  Created by Tony Forsythe on 10/14/16.
+//  Copyright © 2016 Tony Forsythe. All rights reserved.
+//
+
+import UIKit
+
+//  This is the first page of the application. Based on whether or not the user
+//  is logged in it will either display content or prompt them to log in
+
+class LandingPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
